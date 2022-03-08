24c20975ec2db972081b682c6b31046764fea173
Constantin Talandier <constantin.talandier@gmail.com>
2022-03-08 17:11:02 +0100

    Change to instroduce rendement

--- deepagri/data_full.py
+++ deepagri/data_full.py
@@ -1,7 +1,8 @@
+from webbrowser import get
 import pandas as pd
 from deepagri.data_pop import get_data_population
 from deepagri.data_price import get_prices
-from deepagri.data_prod import get_production
+from deepagri.data_prod import get_production, get_df_all
 from deepagri.meteo_agg import agg_meteo
 from deepagri.data_rendement import get_data_rendement
 from deepagri.data_region_ohe import ohe_regions, ohe_cluster_prod, ohe_13_regions
@@ -9,7 +10,7 @@ import os
 
 PATH=os.path.dirname(os.path.dirname(__file__))
 
-def get_df_full(agg_type='S', ohe='full', **kwargs):
+def get_df_full(agg_type='S', ohe='full',what='rendement', **kwargs):
     '''
     Return a dataframe with all the data of meteo of the year and the year before,
     prices ratios, population, yield n-1 and price of the matos.
@@ -24,7 +25,7 @@ def get_df_full(agg_type='S', ohe='full', **kwargs):
     '''
     df_pop=get_data_population()
     df_prices=get_prices()
-    df_prod=get_production()
+    df_prod=get_df_all(what=what)
     df_meteo=agg_meteo(agg_type=agg_type, **kwargs)
     df_rendement=get_data_rendement()
     df_matos=pd.read_csv(os.path.join(os.path.join(PATH,'raw_data'),'matos.csv'),index_col='0')
@@ -52,4 +53,4 @@ def get_df_full(agg_type='S', ohe='full', **kwargs):
     return df
 
 if __name__=='__main__':
-    print(get_df_full('W'))
+    print(get_df_full('M'))
