98a240978a951f1e4aeebf72e42d7161f7978729
Pierre Cera <pierrecerahuelva@gmail.com>
2022-03-08 12:36:04 +0100

    organizing map and streamlit

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import folium
 import os
 import pandas as pd
 import json
+import numpy as np
 
 st.title('DEEPAGRI - Forecasting French Soft Wheat Production in 2022')
 
@@ -22,22 +23,34 @@ df.set_index('Année',inplace=True)
 df = df.T
 df = df.reset_index()
 
-df.fillna(0,inplace=True)
-
 # Calculating yearly variations in production
 for i in range(2001,2022):
     df[f'Var {i}-{i-1}']=round((df[i]-df[i-1])/df[i-1]*100,2)
 
 df = df.drop([i for i in range(2000,2022)], axis=1)
 
-# Only one year for testing
-df_test = df[['index','Var 2021-2020']]
+# Selecting a year with a slider
+year = st.slider('Select a year', 2001, 2021, step=1)
 
-df_test['index']=df_test['index'].astype(str)
+# Creating the dataframe to plot
+df = df[['index',f'Var {year}-{year-1}']]
+df['index']=df['index'].astype(str)
+df = df.dropna()
 
 # Correcting the problem of '0' with integer below 10
 for i in range(0,9):
-    df_test['index'][i]= '0' + df_test['index'][i]
+    df['index'][i]= '0' + df['index'][i]
+
+#Creating charts
+df_graph = df.copy()
+df_graph.set_index('index', inplace=True)
+df_graph = df_graph.dropna()
+
+chart_data = df_graph[f'Var {year}-{year-1}']
+
+st.area_chart(chart_data)
+
+st.bar_chart(chart_data)
 
 
 # Building map with Folium
@@ -48,43 +61,16 @@ m = folium.Map(location=[47, 1],
                zoom_start=6)
 
 
+
 m.choropleth(
     geo_data=geojson_path,
-    data=df_test,
-    columns=['index', 'Var 2021-2020'],
+    data=df,
+    columns=['index', f'Var {year}-{year-1}'],
     key_on='feature.properties.code',
     fill_color='YlOrBr',
     fill_opacity=0.6,
     line_opacity=0.4,
-    legend_name='Production of Soft Wheat - 2021',
-    bins=[-60, -20, 20, 60, 100, 140, 180, 220])
+    legend_name=f'Production of Soft Wheat - {year}')
 
-
-
-
-
-#def color_function(feat):
-    #return "red" if int(feat["properties"]["code"][:1]) < 5 else "blue"
-
-#folium.GeoJson(
-#    geojson_path,
-#    name="geojson",
-#    style_function=lambda feat: {
-#        "weight": 1,
-#        "color": "black",
-#        "opacity": 0.25,
-#        "fillColor": "blue",
-#        "fillOpacity": 0.25,
-#   },
-#    highlight_function=lambda feat: {
-#        "fillColor": color_function(feat),
-#        "fillOpacity": .5,
-#    },
-#    tooltip=folium.GeoJsonTooltip(
-#        fields=['code', 'nom'],
-#        aliases=['Code', 'Name'],
-#        localize=True
-#    ),
-#).add_to(m)
 
 folium_static(m)
