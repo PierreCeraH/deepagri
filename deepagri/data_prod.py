94a844f518f64af1a25e74e788ad0a1c67a832c3
Wenfang Zhou <wenfangz96@gmail.com>
2022-03-08 16:31:44 +0100

    get_df_all() with a rendement/production/surface option

--- deepagri/data_prod.py
+++ deepagri/data_prod.py
@@ -48,5 +48,68 @@ def get_production():
 
     return df
 
+def get_df_all(what='rendement'):
+    '''
+    Renvoi un df sous forme d'une colonne avec l'index AAAA-D
+    what : soit 'rendement', 'production' ou 'surface'
+    '''
+    filepath = os.path.join(PATH,'Histo_data_YieldsAreasProd.xlsx')
+    df = pd.read_excel(filepath, sheet_name='Blé tendre')
+
+    df = df.rename(columns={'Unnamed: 0':'Année'})
+    df = df.set_index('Année')
+
+    col_rendements = []
+    col_surfaces = []
+    col_prod = []
+
+    for col in df.columns :
+        if '1' in col: #rendements
+            col_rendements.append(col)
+        elif '2' in col: #production
+            col_prod.append(col)
+        else: #surfaces
+            col_surfaces.append(col)
+
+    if what=='rendement':
+        df_return=df[col_rendements].drop(['Année',2022],axis=0)
+    elif what=='production':
+        df_return = df[col_prod].drop(['Année',2022],axis=0)
+    elif what=='surface' :
+        df_return=df[col_surfaces].drop(['Année',2022],axis=0)
+
+    dept_codes = [1,2,3,4,5,6,7,8,9,10,
+              11,12,13,14,15,16,17,
+              18,19,
+              21,22,23,24,25,26,27,28,29,
+              20,
+              30,31,32,33,34,35,36,
+              37,38,39,40,41,42,43,
+              44,45,46,47,48,49,50,
+              51,52,53,54,55,56,57,
+              58,59,60,61,62,63,64,
+              65,66,67,68,69,70,71,
+              72,73,74,75,76,77,78,
+              79,80,81,82,83,84,85,
+              86,87,88,89,90,91,92,
+              93,94,95,'FRANCE',0]
+
+    col_names = df_return.columns
+
+    dict_rename = dict(zip(col_names, dept_codes))
+
+    df_return = df_return.rename(columns=dict_rename)
+
+    df_return = pd.DataFrame(df_return.stack())
+
+    df_return = df_return.rename(columns={0:what})
+    df_return = df_return.reset_index()
+    df_return['Ind']=df_return['Année'].astype(str)+'-'+df_return['level_1'].astype(str)
+
+    df_return = df_return.drop(['level_1','Année'], axis=1)
+    df_return = df_return.set_index('Ind')
+
+    return df_return
+
 if __name__=='__main__':
     print(get_production())
