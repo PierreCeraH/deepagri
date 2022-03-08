c02d518ec4bdbc433187dc900c6ac0b8d24ba809
Wenfang Zhou <wenfangz96@gmail.com>
2022-03-08 15:34:21 +0100

    Added option for production_cluster features instead of department features

--- deepagri/data_region_ohe.py
+++ deepagri/data_region_ohe.py
@@ -1,4 +1,7 @@
+#TODO rename this if nobody using it
 import pandas as pd
+import os
+
 
 def ohe_regions(df:pd.DataFrame()):
 
@@ -6,3 +9,18 @@ def ohe_regions(df:pd.DataFrame()):
     dummies = pd.get_dummies(departements)
 
     return df.join(dummies)
+
+def ohe_cluster_prod(df, cluster=pd.DataFrame()):
+    if cluster.empty:
+        cluster_path=os.path.join(os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            'raw_data'),
+                                  'region_prod_cluster.csv')
+        cluster = pd.read_csv(cluster_path)
+    departments = pd.DataFrame(df.index.astype(str).str[5:].astype(int),
+                               index=df.index, columns=['departments'])
+
+    dpts_clustered = departments.merge(cluster, how='left').set_index(departments.index)
+    dpts_clustered = dpts_clustered.drop(columns="departments")
+
+    return df.join(dpts_clustered)
