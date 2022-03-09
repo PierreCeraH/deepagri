db4d3043bd9d7a7169ea6444d75f8d6c218581ac
Pierre Cera <pierrecerahuelva@gmail.com>
2022-03-09 10:00:00 +0100

    table added on streamlit + image

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import os
 import pandas as pd
 import json
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import misc
 
 geojson_path = 'https://raw.githubusercontent.com/PierreCeraH/deepagri/master/deepagri/data/departements.json?token=GHSAT0AAAAAABRZSTYPRBKUPX6I2JWETXZEYRGDUUQ'
 
@@ -22,9 +24,14 @@ df = df.T
 df = df.reset_index()
 
 # Calculating yearly variations in production
+df_var = df.copy()
 for i in range(2001,2022):
-    df[f'Var {i}-{i-1}']=round((df[i]-df[i-1])/df[i-1]*100,2)
-df = df.drop([i for i in range(2000,2022)], axis=1)
+    df_var[f'Var {i}-{i-1}']=round((df[i]-df[i-1])/df[i-1]*100,2)
+df_var = df_var.drop([i for i in range(2000,2022)], axis=1)
+
+image = '/Users/pierre/code/PierreCeraH/deepagri/Photo-of-a-wheat-field.jpeg'
+st.image(image, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
 
 st.markdown("<h1 style='text-align: center; color: #191970;'>DeepAgri Project</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: #808080;'>Forecasting French Soft Wheat Production in 2022</h3>", unsafe_allow_html=True)
@@ -45,16 +52,17 @@ year = st.slider('Select a year', 2002, 2021, step=1)
 st.markdown('')
 
 # Creating the dataframe to plot
-df = df[['index',f'Var {year}-{year-1}']]
-df['index']=df['index'].astype(str)
-df = df.dropna()
+df_var = df_var[['index',f'Var {year}-{year-1}']]
+df_var['index']=df_var['index'].astype(str)
+df_var = df_var.dropna()
 
 # Correcting the problem of '0' with integer below 10
 for i in range(0,9):
-    df['index'][i]= '0' + df['index'][i]
+    df_var['index'][i]= '0' + df_var['index'][i]
 
+# ---------------------------------------------------
 #Creating charts
-df_graph = df.copy()
+df_graph = df_var.copy()
 df_graph = df_graph.drop(0,axis=0)
 df_graph['index']=df_graph['index'].astype(str)
 df_graph.set_index('index', inplace=True)
@@ -65,7 +73,7 @@ chart_data = df_graph[f'Var {year}-{year-1}']
 st.bar_chart(chart_data)
 st.markdown('')
 st.markdown('')
-
+# ---------------------------------------------------
 # Build a DataFrame with Cities and lat long in columns
 def get_map_data():
     return pd.DataFrame(
@@ -74,10 +82,10 @@ def get_map_data():
 
 df_map_cities = get_map_data()
 st.map(df_map_cities)
-
 st.markdown('')
 st.markdown('')
-
+# ---------------------------------------------------
+# Show map with prediction
 col_button = st.columns(3)
 col_name_0 = col_button[0].text('')
 col_name_1 = col_button[1].button('Predict 2022')
@@ -94,7 +102,7 @@ if col_name_1:
 
     m.choropleth(
         geo_data=geojson_path,
-        data=df,
+        data=df_var,
         columns=['index', f'Var {year}-{year-1}'],
         key_on='feature.properties.code',
         fill_color='YlOrBr',
@@ -102,17 +110,53 @@ if col_name_1:
         line_opacity=0.4,
         legend_name=f'Production of Soft Wheat - {year}')
     folium_static(m)
+# ---------------------------------------------------
+# Building a table with chosen department and prediction
+liste_noms_dept = ['01 - Ain','02 - Aisne','03 - Allier','04 - Alpes-de-Haute-Provence',
+                   '05 - Hautes-Alpes','06 - Alpes-Maritimes','07 - Ardèche','08 - Ardennes',
+                   '09 - Ariège','10 - Aube','11 - Aude','12 - Aveyron',
+                   '13 - Bouches-du-Rhône','14 - Calvados','15 - Cantal','16 - Charente',
+                   '17 - Charente-Maritime','18 - Cher','19 - Corrèze',
+                   "21 - Côte-d'Or","22 - Côtes-d'Armor","23 - Creuse","24 - Dordogne",
+                   "25 - Doubs","26 - Drôme","27 - Eure","28 - Eure-et-Loir",
+                   "29 - Finistère","30 - Gard","31 - Haute-Garonne","32 - Gers",
+                   "33 - Gironde","34 - Hérault","35 - Ille-et-Vilaine","36 - Indre",
+                   "37 - Indre-et-Loire","38 - Isère","39 - Jura","40 - Landes",
+                   "41 - Loir-et-Cher","42 - Loire","43 - Haute-Loire","44 - Loire-Atlantique",
+                   "45 - Loiret","46 - Lot","47 - Lot-et-Garonne","48 - Lozère",
+                   "49 - Maine-et-Loire","50 - Manche","51 - Marne","52 - Haute-Marne",
+                   "53 - Mayenne","54 - Meurthe-et-Moselle","55 - Meuse","56 - Morbihan",
+                   "57 - Moselle","58 - Nièvre","59 - Nord","60 - Oise",
+                   "61 - Orne","62 - Pas-de-Calais","63 - Puy-de-Dôme","64 - Pyrénées-Atlantiques",
+                   "65 - Hautes-Pyrénées","66 - Pyrénées-Orientales","67 - Bas-Rhin","68 - Haut-Rhin",
+                   "69 - Rhône","70 - Haute-Saône","71 - Saône-et-Loire","72 - Sarthe",
+                   "73 - Savoie","74 - Haute-Savoie","76 - Seine-Maritime","77 - Seine-et-Marne",
+                   "79 - Deux-Sèvres","80 - Somme","81 - Tarn","82 - Tarn-et-Garonne",
+                   "83 - Var","84 - Vaucluse","85 - Vendée","86 - Vienne",
+                   "87 - Haute-Vienne","88 - Vosges","89 - Yonne","90 - Territoire de Belfort",
+                   "91 - Essonne","92 - Hauts-de-Seine","93 - Seine-Saint-Denis","94 - Val-de-Marne",
+                   "95 - Val-d'Oise"]
 
-#params = {
-#    'pickup_datetime' : pickup_datetime,
-#    'pickup_longitude' : pickup_longitude,
-#    'pickup_latitude' : pickup_latitude,
-#    'dropoff_longitude' : dropoff_longitude,
-#    'dropoff_latitude' : dropoff_latitude,
-#    'passenger_count' : passenger_count
-#}
-#url = 'https://taxifare.lewagon.ai/predict'
+# Creating the dataframe to plot
+df_graph_dept = df.copy()
+df_graph_dept['index']=df_graph_dept['index'].astype(str)
+df_graph_dept = df_graph_dept.dropna()
+# Correcting the problem of '0' with integer below 10
+for i in range(0,9):
+    df_graph_dept['index'][i]= '0' + df_graph_dept['index'][i]
+df_graph_dept.set_index('index',inplace=True)
 
-#if bt:
-#    response = requests.get(url, params).json()
-#    st.write(response['fare'])
+option = st.selectbox('Select a department', liste_noms_dept)
+opt_num = option[:2]
+
+df_graph_dept = df_graph_dept.loc[opt_num,:]
+
+st.bar_chart(df_graph_dept)
+
+
+
+
+# TO DO :
+# Add the predictions for 2022 to df
+# Link the model to the predict button
+# Link df to the table
