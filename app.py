f10d46e15ec0176f168123355396d24fa7263073
Vincent Moreau <vincent.moreau@protonmail.com>
2022-03-10 22:10:24 +0100

    add app for department view

--- app.py
+++ app.py
@@ -14,10 +14,8 @@ import numpy as np
 import altair as alt
 import seaborn as sns
 import time
-
-
-st.session_state.pred_done = False
-
+from apps import department_pred
+details = False
 # ------------------------------------------------------------------------------
 # DEF API FUNCTION
 # ------------------------------------------------------------------------------
@@ -289,67 +287,17 @@ def show_predict():
 
     folium_static(m)
 
-    st.session_state.pred_done = True
-
-    return df_var, df_final
-
-#@st.cache(suppress_st_warning=True)
-def show_table():
-
-    liste_noms_dept = ['00 - FRANCE','01 - Ain','02 - Aisne','03 - Allier','04 - Alpes-de-Haute-Provence',
-                '05 - Hautes-Alpes','06 - Alpes-Maritimes','07 - Ardèche','08 - Ardennes',
-                '09 - Ariège','10 - Aube','11 - Aude','12 - Aveyron',
-                '13 - Bouches-du-Rhône','14 - Calvados','15 - Cantal','16 - Charente',
-                '17 - Charente-Maritime','18 - Cher','19 - Corrèze',
-                "21 - Côte-d'Or","22 - Côtes-d'Armor","23 - Creuse","24 - Dordogne",
-                "25 - Doubs","26 - Drôme","27 - Eure","28 - Eure-et-Loir",
-                "29 - Finistère","30 - Gard","31 - Haute-Garonne","32 - Gers",
-                "33 - Gironde","34 - Hérault","35 - Ille-et-Vilaine","36 - Indre",
-                "37 - Indre-et-Loire","38 - Isère","39 - Jura","40 - Landes",
-                "41 - Loir-et-Cher","42 - Loire","43 - Haute-Loire","44 - Loire-Atlantique",
-                "45 - Loiret","46 - Lot","47 - Lot-et-Garonne","48 - Lozère",
-                "49 - Maine-et-Loire","50 - Manche","51 - Marne","52 - Haute-Marne",
-                "53 - Mayenne","54 - Meurthe-et-Moselle","55 - Meuse","56 - Morbihan",
-                "57 - Moselle","58 - Nièvre","59 - Nord","60 - Oise",
-                "61 - Orne","62 - Pas-de-Calais","63 - Puy-de-Dôme","64 - Pyrénées-Atlantiques",
-                "65 - Hautes-Pyrénées","66 - Pyrénées-Orientales","67 - Bas-Rhin","68 - Haut-Rhin",
-                "69 - Rhône","70 - Haute-Saône","71 - Saône-et-Loire","72 - Sarthe",
-                "73 - Savoie","74 - Haute-Savoie","76 - Seine-Maritime","77 - Seine-et-Marne",
-                "79 - Deux-Sèvres","80 - Somme","81 - Tarn","82 - Tarn-et-Garonne",
-                "83 - Var","84 - Vaucluse","85 - Vendée","86 - Vienne",
-                "87 - Haute-Vienne","88 - Vosges","89 - Yonne","90 - Territoire de Belfort",
-                "91 - Essonne","93 - Seine-Saint-Denis","94 - Val-de-Marne","95 - Val-d'Oise"]
-
-    option = st.selectbox('Select a department', liste_noms_dept)
-    opt_num = option[:2]
-
-# ------------------------------------------------------------------------------
-# PLOTTING THE EVOLUTION OF A CHOSEN DEPARTMENT PRODUCTION
-# ------------------------------------------------------------------------------
-
-    fig = plt.figure(figsize=(10, 4))
-    plt.ylabel(f'{option[3:]}')
-    clrs = ['#ffb482' if (x == 2022) else '#4c72b0' for x in df_final.columns]
-    values = np.array(df_final.loc['00'])
-    idx = np.array(df_final.columns)
-
-    sns.barplot(x=idx, y=values, palette=clrs)
-    st.pyplot(fig)
-
-    st.markdown("<h6 style='text-align: center; color: #708090;'>DeepAgri Project - Le Wagon - Data Science - Batch #802</h6>", unsafe_allow_html=True)
-    columns_names = st.columns(4)
-    col_name_0 = columns_names[0].markdown("<h7 style='text-align: center; color: #708090;'>Pierre Cera-Huelva</h7>", unsafe_allow_html=True)
-    col_name_1 = columns_names[1].markdown("<h7 style='text-align: center; color: #708090;'>Gaspar Dupas</h7>", unsafe_allow_html=True)
-    col_name_2 = columns_names[2].markdown("<h7 style='text-align: center; color: #708090;'>Constantin Talandier</h7>", unsafe_allow_html=True)
-    col_name_3 = columns_names[3].markdown("<h7 style='text-align: center; color: #708090;'>Wenfang Zhou</h7>", unsafe_allow_html=True)
+    st.session_state.df_final = df_final
 
 if bt:
-    df_var, df_final = show_predict()
+    show_predict()
 
-if st.session_state.pred_done:
-    show_table()
-
-
+if "department_view" not in st.session_state:
+    details = st.button('View per Department')
+    
+if details or "department_view" in st.session_state:
+    st.session_state.department_view = True
+    department_pred.show_table(st.session_state.df_final)
 
 # if st.button("prediction"):
 
